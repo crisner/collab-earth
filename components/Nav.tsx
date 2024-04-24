89cc882ce7462ec9efa209c756cc37678b312f8c
Renisha Christie <christie.reni@gmail.com>
2024-04-24 22:52:22 +0530

    Fix menu

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -1,42 +1,50 @@
 import React from "react";
 import { Button } from "@/components/ui/button";
-import { AlignLeft } from "lucide-react";
+import { AlignLeft, ChevronsLeft } from "lucide-react";
 import {
-    SheetContent,
-    SheetHeader,
-    SheetTitle,
-    SheetTrigger,
-  } from "@/components/ui/sheet"
-  import Logo from "@/public/logo.svg";
+  Sheet,
+  SheetClose,
+  SheetContent,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "@/components/ui/sheet";
+import Logo from "@/public/logo.svg";
+import styles from "./Nav.module.css";
 
 const Nav = () => {
   return (
     <div className="py-4 ml-4 ">
       <Sheet>
-          <SheetTrigger asChild>
+        <SheetTrigger asChild>
           <Button variant="outline" size="icon" className="rounded-full">
-      <AlignLeft />
-      </Button>
-          </SheetTrigger>
-          <SheetContent side='left'>
-            <SheetHeader>
-              <SheetTitle><Logo className='logo' /></SheetTitle>
-            </SheetHeader>
-            <div className="grid gap-4 py-4">
-              <div className="grid grid-cols-4 items-center gap-4">
-              <Button variant="link" >Profile</Button>
-              </div>
-              <div className="grid grid-cols-4 items-center gap-4">
-              <Button variant="link" >Notes</Button>
-              </div>
-              <div className="grid grid-cols-4 items-center gap-4">
-              <Button variant="link" >Signout</Button>
-              </div>
-              
-            </div>
-            
-          </SheetContent>
-        </Sheet>
+            <AlignLeft />
+          </Button>
+        </SheetTrigger>
+        <SheetContent className={`nav ${styles.nav}`} side="left">
+          <SheetHeader>
+            <SheetTitle>
+              <Logo className="logo" />
+            </SheetTitle>
+          </SheetHeader>
+          <div className="grid gap-4 py-4">
+            <Button variant="ghost" className="justify-start">
+              Profile
+            </Button>
+            <Button variant="ghost" className="justify-start">
+              Notes
+            </Button>
+            <Button variant="ghost" className="justify-start">
+              Signout
+            </Button>
+          </div>
+          <SheetClose className={styles.collapse} asChild>
+            <Button variant="ghost" size="icon" className="rounded-full">
+              <ChevronsLeft />
+            </Button>
+          </SheetClose>
+        </SheetContent>
+      </Sheet>
     </div>
   );
 };

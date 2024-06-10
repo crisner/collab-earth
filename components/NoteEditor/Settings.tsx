d8ff5d7af5b8bd914b5be40acbf681291636cb7f
Renisha Christie <christie.reni@gmail.com>
2024-06-10 22:27:57 +0530

    Add input features to note settings

--- components/NoteEditor/Settings.tsx
+++ components/NoteEditor/Settings.tsx
@@ -1,3 +1,5 @@
+'use client';
+
 import { Button } from "@/components/ui/button"
 import {
   DropdownMenu,
@@ -8,7 +10,9 @@ import {
   DropdownMenuSeparator,
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu"
-import { Settings2 } from "lucide-react"
+import { Minus, Send, Settings2, SquarePlus } from "lucide-react"
+import { Switch } from "../ui/switch"
+import { Input } from "../ui/input";
 
 export default function NoteSettings() {
   return (
@@ -20,20 +24,24 @@ export default function NoteSettings() {
         <DropdownMenuLabel>Note Settings</DropdownMenuLabel>
         <DropdownMenuSeparator />
         <DropdownMenuGroup>
-          <DropdownMenuItem>
-            Sightings
+          <DropdownMenuItem className="block">
+            <div className="flex justify-between">Sightings<Button variant='ghost' className="h-4"><SquarePlus className="h-4 w-4" /></Button></div>
+            <div className="space-y-2 mt-2 block">
+            <div className="flex"><Input className="h-8" /><Button variant='outline' size='icon' className="h-8"><Minus  className="h-4 w-4" /></Button></div>
+            </div>
           </DropdownMenuItem>
-          <DropdownMenuItem>
-            Make Public
+          <DropdownMenuSeparator />
+          <DropdownMenuItem className="flex justify-between items-center space-x-2">
+            Make Public <Switch className="settings-switch w-8 h-5" checked={false} onCheckedChange={()=>{}} />
           </DropdownMenuItem>
-          <DropdownMenuItem>
-            Anyone can contribute
-          </DropdownMenuItem>
-          <DropdownMenuItem>
-            Invite collaborators
+          <DropdownMenuSeparator />
+          <DropdownMenuItem  className="flex justify-between items-center space-x-2">
+            Anyone can contribute <Switch className="settings-switch w-8 h-5" checked={true} onCheckedChange={()=>{}} />
+          </DropdownMenuItem >
+          <DropdownMenuItem className="block">
+            <div className="flex justify-between items-center">Invite contributers<Button variant='ghost' className="h-4" size='icon'><Send className="h-4 w-4" /></Button></div>
           </DropdownMenuItem>
         </DropdownMenuGroup>
-        <DropdownMenuSeparator />
       </DropdownMenuContent>
     </DropdownMenu>
   )
